29af801edaaed096ceaeef7e84d3a7d1bd2ce9da
David Viteri <davidteri91@gmail.com>
2015-08-04 20:23:03 +0200

    sirius: update fingerprint to latest lollipop release
    Change-Id: Icb88cfc56116957e37dcbae13da1e9c459ec4d1c
    Signed-off-by: David Viteri <davidteri91@gmail.com>
    
    Conflicts:
    	slim.mk

--- slim.mk
+++ slim.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 $(call inherit-product, device/sony/sirius/full_sirius.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:5.0.2/23.1.A.0.690/814068233:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 5.0.2 23.1.A.0.690 814068233 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:5.1.1/23.4.A.0.546/864948651:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 5.1.1 23.4.A.0.546 864948651 release-keys"
 
 PRODUCT_NAME := slim_sirius
 PRODUCT_DEVICE := sirius
